ebfb428ffae04b9bb78e6c2231bbd60a133d817c
soso1554848 <benz560701@gmail.com>
2023-03-16 21:34:26 +0800

    w05

--- demo/w05-modal-starter/script_31.js
+++ demo/w05-modal-starter/script_31.js
@@ -13,6 +13,11 @@ const openModal = (index) => {
     overlay.classList.remove('hidden');
 }
 
+const closeModal = () =>{
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
 for(let i=0; i<btnsOpenModal.length; i++ ) {
     btnsOpenModal[i].addEventListener('click', openModal(i));
 }
@@ -21,5 +26,9 @@ document.addEventListener('keydown', (e) => {
     if (e.key === 'ESCAPE' && !modal.classList.contains('hidden')){
         closeModal();
     }
+});
+
+btnCloseModal.addEventListener('click',(a) => {
+   closeModal(); 
 });
 //btnsOpenModal.forEach(() => );
